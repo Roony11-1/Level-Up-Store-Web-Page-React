e48c21972e502aebd9ac9d071a4bf3ffd775dd2d
Roony11-1 <eltheasasin@gmail.com>
2025-10-02 20:32:25 -0300

    Primer Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Holaaaaa =)
+<br>
+git config --global user.email  "CORREO"
+<br>
+git config --global user.name  "NOMBRE USU"
+
+Cosas instaladas<br><br>
+
+npm install react-router-dom
+-- Enrrutamiento de las páginas
---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+
+// Componente para manejar rutas no encontradas
+function NotFound() {
+  return (
+    <div>
+      <h1>404</h1>
+      <p>Página no encontrada</p>
+    </div>
+  );
+}
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/app" element={<App />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
