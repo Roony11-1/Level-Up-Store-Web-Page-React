28bb87d85d84f4daec2754fed0746e942ae84120
Roony11-1 <eltheasasin@gmail.com>
2025-10-03 09:56:04 -0300

    Añadido grid

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,16 @@
 import '../../assets/css/Header/header.css';
 
+import { HeaderLogo } from './HeaderLogo';
+
 export function Header() 
 {
   return(
-    <header>Header Component</header>
+    <header>
+      <div className="parent">
+          <div className="izquierda"><HeaderLogo /></div>
+          <div className="central">2</div>
+          <div className="derecha">3</div>
+      </div>
+    </header>
   )
 }
