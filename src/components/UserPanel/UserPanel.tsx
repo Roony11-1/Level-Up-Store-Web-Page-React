f029c3ba7038a1904190c2fc44937c7dc914e016
Roony11-1 <eltheasasin@gmail.com>
2025-10-03 11:29:42 -0300

    Añadido esqueleto

--- src/components/UserPanel/UserPanel.tsx
+++ src/components/UserPanel/UserPanel.tsx
@@ -1,10 +1,21 @@
+import { Link } from 'react-router-dom';
 import '../../assets/css/UserPanel/userpanel.css';
 
 export function UserPanel() 
 {
   return(
     <section className="user-panel">
-        Panel Usuario
+      <h1>Panel Usuario</h1>
+      <hr /><br />
+      <div>
+        <Link to={'/'}><h2>Inicio</h2></Link>
+        <Link to={'/productos'}><h2>Productos</h2></Link>
+        <Link to={'/nosotros'}><h2>Nosotros</h2></Link>
+        <Link to={'/blogs'}><h2>Blogs</h2></Link>
+        <Link to={'/contactos'}><h2>Contactos</h2></Link>
+      </div>
+      <Link to={'/app'}><h2>App</h2></Link>
+        
     </section>
   )
 }
