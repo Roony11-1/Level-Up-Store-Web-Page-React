3615b0d5702277c1a639758a81de6b3e26eee806
Roony11-1 <eltheasasin@gmail.com>
2025-10-03 00:00:43 -0300

    Movido index.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
+import { Layout } from "./components/Layout/Layout.tsx";
 import App from "./App.tsx";
+import "./assets/css/index.css";
 
 // Paginas
 import { Index } from "./pages/Index.tsx";
@@ -21,9 +22,11 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/app" element={<App />} />
-        <Route path="/" element={<Index />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Index />} />
+          <Route path="app" element={<App />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
