75682d0759aaacdeef743e8df5611846e0e69fa7
Roony11-1 <eltheasasin@gmail.com>
2025-10-04 13:18:44 -0300

    Añadido campo contraseña

--- src/components/DisplayUser/DisplayUser.tsx
+++ src/components/DisplayUser/DisplayUser.tsx
@@ -6,6 +6,7 @@ export function DisplayUser({ usuario }: { usuario: Usuario }) {
         <div style={{ border: "1px solid black", margin: "10px", padding: "10px" }}>
             <p>Id: {usuario.getId()}</p>
             <p>Nombre: {usuario.getNombreUsuario()}</p>
+            <p>Contraseña: {usuario.getContraseña()}</p>
             <p>Email: {usuario.getEmail()}</p>
             <p>Tipo: {usuario.getTipo()}</p>
             <p>Teléfono: {usuario.getTelefono()}</p>
