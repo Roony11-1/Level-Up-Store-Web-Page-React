be741b7c7cde89eec7be891f4ff7b628d2079fb0
Roony11-1 <eltheasasin@gmail.com>
2025-10-04 12:59:44 -0300

    Component para hacer display a los usuarios

---
+++ src/components/DisplayUser/DisplayUser.tsx
@@ -0,0 +1,16 @@
+import { Usuario } from "../../model/Usuario";
+
+
+export function DisplayUser({ usuario }: { usuario: Usuario }) {
+    return (
+        <div style={{ border: "1px solid black", margin: "10px", padding: "10px" }}>
+            <p>Id: {usuario.getId()}</p>
+            <p>Nombre: {usuario.getNombreUsuario()}</p>
+            <p>Email: {usuario.getEmail()}</p>
+            <p>Tipo: {usuario.getTipo()}</p>
+            <p>Teléfono: {usuario.getTelefono()}</p>
+            <p>Región: {usuario.getRegion()}</p>
+            <p>Comuna: {usuario.getComuna()}</p>
+        </div>
+    );
+}
