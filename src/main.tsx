bebf888e9353760d9aaf5feb8b8bd50ec56a7a36
CHOCOLATIN-12345 <vict.sandovalc@duocuc.cl>
2025-10-09 09:33:49 -0300

    dsfd

--- src/main.tsx
+++ src/main.tsx
@@ -11,11 +11,8 @@ import { NotFound } from "./pages/NotFound.tsx";
 import { PruebaApi } from "./pages/PruebaApi.tsx";
 import { Productos } from "./pages/Productos.tsx";
 import { Login } from "./pages/Login.tsx";
-<<<<<<< HEAD
 import { Nosotros } from "./pages/Nosotros.tsx";
-=======
 import { Registro } from "./pages/Registro.tsx";
->>>>>>> 4b0459bb4ab9987032fd2d10977c9f18706fcdc5
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
