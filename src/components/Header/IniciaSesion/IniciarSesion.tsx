2ceba38dff8cee58c1d0f834a7cb2466bf1c66b0
Roony11-1 <eltheasasin@gmail.com>
2025-10-09 17:29:40 -0300

    iniciar sesion

--- src/components/Header/IniciaSesion/IniciarSesion.tsx
+++ src/components/Header/IniciaSesion/IniciarSesion.tsx
@@ -1,9 +1,19 @@
+import { Link } from "react-router-dom"
+import "../../../assets/css/Header/IniciarSesion/iniciarsesion.css"
+
 export function IniciarSesion()
 {
     return(
-        <div>
+        <div className="sesion-header">
             <h2>Hola!</h2>
-            <p>Inicia Sesión</p>
+            <p>Ingresa a tu cuenta!</p>
+
+        <div className="sesion-panel">
+            <ul>
+            <Link to={'/login'}><h2>Iniciar Sesión</h2></Link>
+            <Link to={'/registrarse'}><h2>Registrarse</h2></Link>
+            </ul>
+        </div>
         </div>
     )
 }
