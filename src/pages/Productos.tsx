d17b45277c6e9e9755306006d958be43d7fb4ca0
Neytan2214 <ney.silva@duocuc.cl>
2025-10-09 10:23:54 -0300

    ---se mejora Productos.tsx y se hace para que se pueda visualizar en el main---

--- src/pages/Productos.tsx
+++ src/pages/Productos.tsx
@@ -1,8 +1,25 @@
+import { Producto } from "../model/Producto";
+import { useEffect } from "react";
+import { useState } from "react";
+import { DisplayProduct } from "../components/DisplayProduct/DisplayProduct";
+import { ProductoApiService } from "../services/ProductoApiService"; 
 export function Productos() {
+    const [productos, setProductos] = useState<Producto[]>([])
+        const productoService = new ProductoApiService();
+    
+        useEffect(() => {
+            const fetchProductos = async () => {
+                const datos = await productoService.fetchAll();
+                setProductos(datos);
+              };
+            
+              fetchProductos();
+        }, []);
     return (
-        <div>
-            <h1>Catálogo</h1>
-            <p>Bienvenido a la página de el catálogo.</p>
-        </div>
+          <div>
+                  {productos.map((p) => (
+                    <DisplayProduct producto={p} />
+                  ))}
+                </div>
     );
 }
