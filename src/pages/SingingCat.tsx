09923c461f77c54632444147ef7c774c129fad7f
Roony11-1 <eltheasasin@gmail.com>
2025-10-11 18:04:17 -0300

    Quitada posibilidad de pausar

--- src/pages/SingingCat.tsx
+++ src/pages/SingingCat.tsx
@@ -5,7 +5,6 @@ export function SingingCat()
     return (
         <video 
             src={videoGato} 
-            controls 
             autoPlay={true} 
             loop={true} 
             muted={false} 
