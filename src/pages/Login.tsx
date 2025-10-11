ac36632ffad0e08ba8cad4d1faa9163e75c14850
Roony11-1 <eltheasasin@gmail.com>
2025-10-10 21:40:48 -0300

    Mejorado logeo (Por ahora no inicia una sesion)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,25 +3,64 @@ import { LoginRequest } from "../model/LoginRequest";
 import { UsuarioApiService } from "../services/UsuarioApiService";
 
 import "../assets/css/Login/login.css"
+import { FormInput } from "../components/Formularios/FormInput/Forminput";
 
 export function Login() 
 {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => 
+    {
+        const { name, value } = e.target;
+        setFormData((prev) => ({ ...prev, [name]: value }));
+    };
+
+    function validarFormRegistro(formData: any): string[] 
+    {
+        const errores: string[] = [];
+
+        if (!formData.email)
+            errores.push("El correo es obligatorio.");
+
+        if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(formData.email))
+            errores.push("El correo electrónico no es válido.");
+
+        if (!formData.password)
+            errores.push("La contraseña es obligatoria.");
+
+        return errores;
+    }
 
     const handleSubmit = async (e: React.FormEvent) => 
     {
         e.preventDefault();
 
-        const loginRequest: LoginRequest = new LoginRequest(email, password);
+        const errores = validarFormRegistro(formData);
 
+        if (errores.length > 0) 
+        {
+            alert("Errores:\n" + errores.join("\n"));
+            return;
+        }
 
-        const usuarioLogin = await new UsuarioApiService().login(loginRequest);
+        const loginRequest: LoginRequest = new LoginRequest(formData.email, formData.password);
+        const usuarioService = new UsuarioApiService();
 
-        if (usuarioLogin)
-            alert(`Email: ${usuarioLogin.getEmail()}, Password: ${usuarioLogin.getContraseña()}`);
-        else
-            alert("Error en el login");
+        const resultado = await usuarioService.login(loginRequest);
+
+        if (resultado.success) 
+        {
+            setFormData({
+                email: "",
+                password: ""
+            });
+            
+        }
+        
+        alert(resultado.message);
     };
 
     return (
@@ -30,25 +69,16 @@ export function Login()
             <div className="login-container">
                 <form onSubmit={handleSubmit}>
                     <table>
-                        <tr>
-                            <td>Email: </td>
-                            <td>
-                                <input
-                                    placeholder="Correo Electrónico"
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    value={email}/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Contraseña: </td>
-                            <td>
-                                <input
-                                    type="password"
-                                    placeholder="Contraseña"
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    value={password}/>
-                            </td>
-                        </tr>
+                    <FormInput 
+                        name='email'
+                        label='Correo Electrónico'
+                        onChange={handleChange}
+                        value={formData.email} />
+                    <FormInput 
+                        name='password'
+                        label='Contraseña'
+                        onChange={handleChange}
+                        value={formData.password} />
                         <tr>
                             <td colSpan={2}>
                                 <button type="submit">Iniciar Sesión</button>
