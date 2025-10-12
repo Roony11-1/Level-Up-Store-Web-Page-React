5ce4dff6260f752f4f2eddb08de9ae2f106c569a
Roony11-1 <eltheasasin@gmail.com>
2025-10-12 18:44:12 -0300

    singing cat

--- src/pages/SingingCat.tsx
+++ src/pages/SingingCat.tsx
@@ -1,13 +1,17 @@
 import videoGato from "../assets/vid/snail car daily.mp4"
 
+import "../assets/css/SingingCat/singingcat.css"
+
 export function SingingCat() 
 {
     return (
-        <video 
-            src={videoGato} 
-            autoPlay={true} 
-            loop={true} 
-            muted={false} 
-        />
+        <div className="video-container">
+            <video 
+                src={videoGato} 
+                autoPlay={true} 
+                loop={true} 
+                muted={false} 
+            />
+        </div>
     );
 }
