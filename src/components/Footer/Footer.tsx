2233157b00bcdf71689826e72be1a2aeedce1581
CHOCOLATIN-12345 <vict.sandovalc@duocuc.cl>
2025-10-14 09:16:55 -0300

    se arreglo footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,22 @@
+import { NavLink } from 'react-router-dom';
 import '../../assets/css/Footer/footer.css';
 
-export function Footer() 
-{
-  return(
+
+export function Footer() {
+  return (
+
     <footer>
-        <h1>Soy un Footer</h1>
+      <div className="Footer">
+        <div className="div2">
+        <NavLink to="nosotros" ><h2>Nosotros</h2></NavLink>
+        <NavLink to="contacto" ><h2>Contacto</h2></NavLink>
+        </div>
+
+        <div className="div3">
+        
+        </div>
+      </div>
+
     </footer>
   )
 }
