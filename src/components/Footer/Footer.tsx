e5ab635dbb3e8b79758d9a4481df31a8ef86fcde
Neytan2214 <ney.silva@duocuc.cl>
2025-10-14 10:04:18 -0300

    ---se mejoro el diseño del footer---

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import '../../assets/css/Footer/footer.css';
+import'../../css/estilos.css'
 
 
 export function Footer() {
@@ -9,13 +10,38 @@ export function Footer() {
       <div className="Footer">
         <div className="div2">
         <NavLink to="nosotros" ><h2>Nosotros</h2></NavLink>
-        <NavLink to="contacto" ><h2>Contacto</h2></NavLink>
+        <NavLink to="/contacto" ><h2>Contacto</h2></NavLink>
         </div>
 
         <div className="div3">
-        
+        {/* Columna izquierda: Título y descripción */}
+        <div className="footer-col">
+          <h3 className="footer-title">Level-Up Store © 2025</h3>
+          <p className="footer-text">
+            Tu tienda de confianza para todo lo relacionado con videojuegos.
+          </p>
         </div>
-      </div>
+
+        {/* Columna central: Categorías y enlaces */}
+        <div className="footer-col">
+          <p className="footer-text">
+            Categorías: Consolas, Juegos, Accesorios, Merchandising
+          </p>
+        </div>
+
+          {/* Columna derecha: Newsletter */}
+          <div className="footer-col">
+            <p className="footer-text">Contacto:</p>
+            <p className="footer-text">Suscríbete a nuestra newsletter</p>
+            <label className="footer-text">
+              Tu Email:
+              <input type="email" placeholder="email@example.com" className="footer-input" />
+            </label>
+          </div>
+          </div>
+          </div>
+
+     
 
     </footer>
   )
