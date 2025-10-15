d9c9010576e1bf0ab602ac02abecb4e89c617a06
Roony11-1 <eltheasasin@gmail.com>
2025-10-15 06:53:08 -0300

    ta entero feo pero puedo cerrar la sesion

--- src/components/Header/IniciaSesion/IniciarSesion.tsx
+++ src/components/Header/IniciaSesion/IniciarSesion.tsx
@@ -1,17 +1,50 @@
 import { Link } from "react-router-dom"
 import "../../../assets/css/Header/IniciarSesion/iniciarsesion.css"
+import { useSesion } from "../../../context/SesionContext/SesionContext"
+import { Boton } from "../../Boton/Boton";
 
 export function IniciarSesion()
 {
+    const { sesion, sesionLogout } = useSesion();
+
     return(
         <div className="sesion-header">
             <h2>Hola!</h2>
             <p>Ingresa a tu cuenta!</p>
 
-            <div className="sesion-panel">
-                <Link to={'/login'}><h1>Iniciar Sesión</h1></Link>
-                <Link to={'/registrarse'}><h1>Registrarse</h1></Link>
-            </div>
+        {!sesion.getUsuarioActivo() ? <PanelNoLogeado /> : 
+        <PanelLogeado 
+            nombreUsuario={sesion.getUsuarioActivo()?.getNombreUsuario() || ""} 
+            cerrarSesion={sesionLogout}/>}
         </div>
     )
+}
+
+function PanelNoLogeado()
+{
+    return(
+        <div className="sesion-panel">
+            <Link to={'/login'}><h1>Iniciar Sesión</h1></Link>
+            <Link to={'/registrarse'}><h1>Registrarse</h1></Link>
+        </div>
+    );
+}
+
+interface PanelLogeadoProps 
+{
+    nombreUsuario: string;
+    cerrarSesion: () => void;
+}
+
+function PanelLogeado({ nombreUsuario, cerrarSesion }: PanelLogeadoProps)
+{
+    return(
+        <div className="sesion-panel">
+            <h2>Bienvenido {nombreUsuario}</h2>
+            <Boton
+                onClick={cerrarSesion}>
+                    Cerrar Sesión
+                </Boton>
+        </div>
+    );
 }
