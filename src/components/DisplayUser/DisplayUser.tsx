07f3a5a708449fe1df70f59f8d1264feee8cadfa
Roony11-1 <eltheasasin@gmail.com>
2025-10-15 17:09:33 -0300

    estilos de displayUser y respectiba tabla

--- src/components/DisplayUser/DisplayUser.tsx
+++ src/components/DisplayUser/DisplayUser.tsx
@@ -1,5 +1,5 @@
 import { Usuario } from "../../model/Usuario";
-
+import "../../assets/css/DisplayUser/displayuser.css"
 
 export function DisplayUser({ usuario }: { usuario: Usuario }) {
     return (
@@ -13,5 +13,54 @@ export function DisplayUser({ usuario }: { usuario: Usuario }) {
             <p>Región: {usuario.getRegion()}</p>
             <p>Comuna: {usuario.getComuna()}</p>
         </div>
+    );
+}
+
+
+
+export function DisplayUserTable({ usuarios }: { usuarios: Usuario[] }) 
+{
+    const handleClick = (e: React.MouseEvent<HTMLTableCellElement>) => 
+    {
+        const celda = e.currentTarget;
+        console.log("Texto:", celda.textContent);
+        console.log("Elemento:", celda);
+    };
+
+    if (!(usuarios.length > 0))
+        return(
+            <h1>No hay usuarios que mostrar!</h1>
+        );
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Contraseña</th>
+                    <th>Email</th>
+                    <th>Tipo</th>
+                    <th>Teléfono</th>
+                    <th>Región</th>
+                    <th>Comuna</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                {usuarios.map((u) => 
+                (
+                    <tr key={u.getId()}>
+                        <td>{u.getId()}</td>
+                        <td><p onClick={handleClick}>{u.getNombreUsuario()}</p></td>
+                        <td><p onClick={handleClick}>{u.getContraseña()}</p></td>
+                        <td><p onClick={handleClick}>{u.getEmail()}</p></td>
+                        <td><p onClick={handleClick}>{u.getTipo()}</p></td>
+                        <td><p onClick={handleClick}>{u.getTelefono() || "-"}</p></td>
+                        <td><p onClick={handleClick}>{u.getRegion() || "-"}</p></td>
+                        <td><p onClick={handleClick}>{u.getComuna() || "-"}</p></td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     );
 }
