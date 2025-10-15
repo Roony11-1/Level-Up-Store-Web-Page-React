97854bfc8dec10221467a0b19bcee204fbb0ef45
Roony11-1 <eltheasasin@gmail.com>
2025-10-15 06:23:00 -0300

    Creado el provider para manejar la sesion

---
+++ src/context/SesionContext/SesionProvider.tsx
@@ -0,0 +1,27 @@
+import { useState, type ReactNode } from "react";
+import { SesionContext } from "./SesionContext";
+import { Sesion } from "../../model/Sesion";
+import type { Usuario } from "../../model/Usuario";
+
+
+export function SesionProvider({ children }: { children: ReactNode })
+{
+    const sesionActiva = new Sesion();
+
+    const [sesion, setSesion] = useState(sesionActiva);
+
+    const sesionLogin = (usuario: Usuario) => 
+    {
+        const nuevaSesion = new Sesion();
+        nuevaSesion.setUsuarioActivo(usuario);
+        setSesion(nuevaSesion);
+    };
+
+    const sesionLogout = () => setSesion(new Sesion());
+
+    return (
+        <SesionContext.Provider value={ { sesion, sesionLogin, sesionLogout } }>
+            {children}
+        </SesionContext.Provider>
+    );
+}
