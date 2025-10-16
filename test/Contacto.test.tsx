1a5b192a846c1575d011ab9e17519ccfa37e9b85
Neytan2214 <ney.silva@duocuc.cl>
2025-10-16 09:54:57 -0300

    ---test Contacto---

---
+++ test/Contacto.test.tsx
@@ -0,0 +1,29 @@
+import{render,screen} from "@testing-library/react";
+import Contacto from "../src/pages/Contacto";
+import userEvent from "@testing-library/user-event";
+import{describe,expect,test} from "vitest";
+describe('testin de formulario',()=>{
+    test('render',()=>{
+        render(<Contacto/>)
+        const titulo=screen
+        .getByRole('heading',{name:/Formulario de Contacto/})
+        expect(titulo).toBeInTheDocument()
+    });
+    test('simular ingreso',async()=>{
+        render(Contacto)
+        const usuario=userEvent.setup()
+
+        const mensaje=screen.getByRole('textbox',{name:/mensaje:/})
+        const nombre=screen.getByRole('textbox',{name:/Nombre:/})
+        const boton=screen.getByRole('button',{name:/enviar:/})
+
+        await usuario.type(nombre,'juan')
+        await usuario.type(mensaje,'hola juan')
+        await usuario.click(boton)
+
+        const alerta=screen.getByRole('alert')
+        expect(alerta).toHaveTextContent(/hola juanito/)
+
+    })
+}
+)
