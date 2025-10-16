4bb5201c03d35583025a7342f60865427f424b86
Roony11-1 <eltheasasin@gmail.com>
2025-10-16 18:21:21 -0300

    Pagina Producto

--- src/components/DisplayProduct/DisplayProduct.tsx
+++ src/components/DisplayProduct/DisplayProduct.tsx
@@ -1,19 +1,15 @@
+import { Link } from "react-router-dom";
 import "../../assets/css/DisplayProduct/displayproduct.css"
 
 import { Producto } from "../../model/Producto";
-import { Boton } from "../Boton/Boton";
 
 
 export function DisplayProduct({ producto }: { producto: Producto }) {
     return (
         <div className="producto">
             <img src={producto.getImagen()} alt={producto.getNombre()}/>
-            <h1>{producto.getNombre()}</h1>
+            <Link to={`/producto?nombre=${producto.getNombre()}`}><h1>{producto.getNombre()}</h1></Link>
             <p>${producto.getPrecio().toLocaleString("es-CL")}</p>
-
-            <Boton>
-                Detalles
-            </Boton>
         </div>
     );
 }
---
+++ src/pages/ProductoPagina.tsx
@@ -0,0 +1,78 @@
+import { useEffect, useMemo, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { ProductoApiService } from "../services/ProductoApiService";
+import { Producto } from "../model/Producto";
+
+import "../assets/css/ProductoPagina/productopagina.css"
+import { Boton } from "../components/Boton/Boton";
+import { RelatedProduct } from "../components/RelatedProduct/RelatedProduct";
+
+export function ProductoPagina()
+{
+    const [producto, setProducto] = useState<Producto | null>();
+    const [productosRelacionados, setProductosRelacionados] = useState<Producto[]>();
+
+    const productoService = useMemo(() => new ProductoApiService(), []);
+
+    const [searchParams] = useSearchParams();
+    const nombre = searchParams.get("nombre");
+
+    useEffect(() => 
+    {
+        if (!nombre) 
+            return;
+
+        // Metodo custom para este componente
+        productoService.fetchProductoConRelacionados(nombre)
+        .then(({ producto, relacionados }) => 
+        {
+            setProducto(producto);
+            setProductosRelacionados(relacionados);
+        });
+    }, [nombre, productoService]);
+
+    if (!producto)
+        return(
+            <h1>No hay productos asociados a ese nombre</h1>
+    );
+
+    return (
+        <div className="displayer-producto">
+            <div className="displayer-imagen">
+                <img
+                    src={producto.getImagen()} />
+            </div>
+            <div className="displayer-info">
+                <div className="info-header">
+                    <strong>{producto.getNombre()}</strong><br />
+                    <strong>{producto.getMarca()}</strong>
+                </div>
+                <div className="info-body">
+                    <p>{producto.getDescripcion()}</p>
+                    <p>${producto.getPrecio().toLocaleString("es-CL")}</p>
+                </div>
+                <div className="info-footer">
+                    <Boton>
+                        Añadir al carrito
+                    </Boton>
+                    <span>Cantidad: {producto.getCantidad()}</span>
+                </div>
+            </div>
+            <div className="displayer-parecidos">
+                <h1>Productos Relacionados</h1>
+                <div className="parecidos">
+                    {productosRelacionados && productosRelacionados.length > 0 ? (
+                        productosRelacionados.map((p) => (
+                            <RelatedProduct key={p.getId()} producto={p} />
+                        ))
+                    ) : (
+                        <p>No hay productos relacionados</p>
+                    )}
+                </div>
+            </div>
+            <div className="displayer-imagenes">
+                <h1>Lista Imagenes Producto</h1>
+            </div>
+        </div>
+    );
+}
