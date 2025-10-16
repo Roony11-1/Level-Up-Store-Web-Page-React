4b7ab49b06d554c8a0bb2d1373b6f747dd0ac329
Roony11-1 <eltheasasin@gmail.com>
2025-10-15 21:26:02 -0300

    Guarda los datos en localStorage

--- src/context/SesionContext/SesionProvider.tsx
+++ src/context/SesionContext/SesionProvider.tsx
@@ -1,26 +1,55 @@
 import { useState, type ReactNode } from "react";
 import { SesionContext } from "./UseSesion";
 import { Sesion } from "../../model/Sesion";
-import type { Usuario } from "../../model/Usuario";
+import { Usuario } from "../../model/Usuario";
 
-
-export function SesionProvider({ children }: { children: ReactNode })
+export function SesionProvider({ children }: { children: ReactNode }) 
 {
-    const sesionActiva = new Sesion();
+    const [sesion, setSesion] = useState(() => 
+    {
+        const datosGuardados = localStorage.getItem("sesion");
+        if (datosGuardados) 
+        {
+            const datos = JSON.parse(datosGuardados);
 
-    const [sesion, setSesion] = useState(sesionActiva);
+            const sesionInicial = new Sesion();
+
+            if (datos.usuarioActivo) 
+            {
+                const u = new Usuario();
+                u.setEmail(datos.usuarioActivo.email);
+                u.setNombreUsuario(datos.usuarioActivo.nombre);
+                u.setTelefono(datos.usuarioActivo.telefono);
+                u.setRegion(datos.usuarioActivo.region);
+                u.setComuna(datos.usuarioActivo.comuna);
+                u.setTipo(datos.usuarioActivo.tipo);
+                u.setProfilePhoto(datos.usuarioActivo.profilePhoto);
+
+                sesionInicial.setUsuarioActivo(u);
+            }
+
+            return sesionInicial;
+        }
+
+        return new Sesion();
+    });
 
     const sesionLogin = (usuario: Usuario) => 
     {
         const nuevaSesion = new Sesion();
         nuevaSesion.setUsuarioActivo(usuario);
+        localStorage.setItem("sesion", JSON.stringify(nuevaSesion));
         setSesion(nuevaSesion);
     };
 
-    const sesionLogout = () => setSesion(new Sesion());
+    const sesionLogout = () => 
+    {
+        localStorage.removeItem("sesion");
+        setSesion(new Sesion());
+    };
 
     return (
-        <SesionContext.Provider value={ { sesion, sesionLogin, sesionLogout } }>
+        <SesionContext.Provider value={{ sesion, sesionLogin, sesionLogout }}>
             {children}
         </SesionContext.Provider>
     );
