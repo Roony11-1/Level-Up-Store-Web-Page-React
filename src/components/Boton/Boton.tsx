216d9d68b2cbbca5f773ecf0cde1713562621198
Roony11-1 <eltheasasin@gmail.com>
2025-10-18 19:05:04 -0300

    type

--- src/components/Boton/Boton.tsx
+++ src/components/Boton/Boton.tsx
@@ -7,12 +7,13 @@ interface BotonProps
     children: ReactNode;
     className?: string;
     onClick?: React.MouseEventHandler<HTMLButtonElement>;
+    type?: "submit";
 }
 
-export function Boton( {children, className="", onClick}:BotonProps )
+export function Boton( {children, className="", onClick, type}:BotonProps )
 {
     return(
-        <button className={`btn ${className}`} onClick={onClick}>
+        <button className={`btn ${className}`} onClick={onClick} type={type}>
             {children}
         </button>
     );
