955c3e8efe46b24aa82ad4af015074cfb8530b79
Roony11-1 <eltheasasin@gmail.com>
2025-10-17 22:28:47 -0300

    Actualizados

--- src/components/Home/DisplayMarkedBlog/DisplayMarkedBlog.tsx
+++ src/components/Home/DisplayMarkedBlog/DisplayMarkedBlog.tsx
@@ -1,10 +1,20 @@
+import { Link } from "react-router-dom";
 import type { Blog } from "../../../model/Blog";
+
+import "../../../assets/css/Home/DisplayMarked/blog.css"
 
 export function DisplayMarkedBlogs( {blog}: {blog: Blog} )
 {
-    return(
-        <div className="blog-destacado">
-            <img src={blog.getImagen()} alt={blog.getTitulo()}/>
-        </div>
+    return (
+        <a
+            href={blog.getUrl()}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="blog-destacado"
+        >
+            <div className="blog-destacado">
+                <img src={blog.getImagen()} alt={blog.getTitulo()} />
+            </div>
+        </a>
     );
 }
--- src/components/Home/DisplayMarkedProduct/DisplayMarkedProduct.tsx
+++ src/components/Home/DisplayMarkedProduct/DisplayMarkedProduct.tsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import type { Producto } from "../../../model/Producto";
+
+import "../../../assets/css/Home/DisplayMarked/product.css"
 
 export function DisplayMarkedProduct({ producto }: { producto: Producto })
 {
     return(
-        <div className="producto-destacado">
+        <Link to={`/producto?nombre=${producto.getNombre()}`} className="producto-destacado">
             <img src={producto.getImagen()} alt={producto.getNombre()}/>
-        </div>
+        </Link>
     );
 }
