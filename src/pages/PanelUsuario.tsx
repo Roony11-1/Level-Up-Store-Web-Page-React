b1fbbf9a81c8f34e318e349ad2c119dbc90fc154
Roony11-1 <eltheasasin@gmail.com>
2025-10-18 18:08:51 -0300

    Loading

--- src/pages/PanelUsuario.tsx
+++ src/pages/PanelUsuario.tsx
@@ -1,34 +1,55 @@
+import { useEffect, useState } from "react";
 import { ProfilePhoto } from "../components/ProfilePhoto/ProfilePhoto";
 import { LoginSecurity } from "../components/Seguridad/LoginSecurity/LoginSecurity";
 import { useSesion } from "../context/SesionContext/UseSesion";
+import type { Usuario } from "../model/Usuario";
+import { UsuarioApiService } from "../services/UsuarioApiService";
 
 export function PanelUsuario()
 {
     const { sesion } = useSesion();
+    const [usuario, setUsuario] = useState<Usuario | null>(null);
+    const [loading, setLoading] = useState(true);
 
-    const usuarioActivo = sesion.getUsuarioActivo();
+    const usuarioService = new UsuarioApiService();
+
+    useEffect(() => 
+    {
+        const fetchUsuario = async () => 
+        {
+            const idUsuarioActivo = sesion.getIdUsuarioActivo();
+
+            if (idUsuarioActivo !== null)
+            {
+                const datos = await usuarioService.fetchById(idUsuarioActivo);
+                setUsuario(datos);
+            }
+            else
+                setUsuario(null);
+
+            setLoading(false)
+        };
+
+        fetchUsuario();
+    }, []);
+
+    if (loading) return <p>Cargando...</p>;
 
     return (
         <LoginSecurity>
-            {usuarioActivo && (
+            {usuario && (
                 <>
                     <div>
-                        <ProfilePhoto profilePhoto={usuarioActivo.getProfilePhoto()} />
+                        <ProfilePhoto profilePhoto={usuario.getProfilePhoto()} />
                     </div>
 
                     <div>
-                        <p><strong>Email:</strong> {usuarioActivo.getEmail() || "No proporcionado"}</p>
-                        <p><strong>Teléfono:</strong> {usuarioActivo.getTelefono() || "No proporcionado"}</p>
-                        <p><strong>Región:</strong> {usuarioActivo.getRegion() || "No proporcionado"}</p>
-                        <p><strong>Comuna:</strong> {usuarioActivo.getComuna() || "No proporcionado"}</p>
-                        <p><strong>Tipo de usuario:</strong> {usuarioActivo.getTipo()}</p>
-                    </div>
-
-                    <div>
-                        <h1>Carritos</h1>
-                        <div>
-                            <h2>Ninguno</h2>
-                        </div>
+                        <p><strong>Nombre de Usuario:</strong> {usuario.getNombreUsuario() || "No proporcionado"}</p>
+                        <p><strong>Email:</strong> {usuario.getEmail() || "No proporcionado"}</p>
+                        <p><strong>Contraseña:</strong> {usuario.getContraseña() || "No proporcionado"}</p>
+                        <p><strong>Teléfono:</strong> {usuario.getTelefono() || "No proporcionado"}</p>
+                        <p><strong>Región:</strong> {usuario.getRegion() || "No proporcionado"}</p>
+                        <p><strong>Comuna:</strong> {usuario.getComuna() || "No proporcionado"}</p>
                     </div>
                 </>
             )}
