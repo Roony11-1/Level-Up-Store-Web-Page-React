e4178aeda5e00c42cbead4aed1dd18f510372745
Roony11-1 <eltheasasin@gmail.com>
2025-10-21 20:56:51 -0300

    uso appProviders

--- src/AppProviders.tsx
+++ src/AppProviders.tsx
@@ -2,13 +2,16 @@ import type { ReactNode } from "react";
 import { CarritoProvider } from "./context/CarritoContext/CarritoProvider";
 import { ProductoServiceProvider } from "./context/ProductoServiceContext/ProductoServiceProvider";
 import { SesionProvider } from "./context/SesionContext/SesionProvider";
+import { UsuarioServiceProvider } from "./context/UsuarioServiceContext/UsuarioServiceProvider";
 
 export const AppProviders = ({ children }: { children: ReactNode }) => (
-  <ProductoServiceProvider>
-    <SesionProvider>
-      <CarritoProvider>
-        {children}
-      </CarritoProvider>
-    </SesionProvider>
-  </ProductoServiceProvider>
+    <UsuarioServiceProvider>
+        <ProductoServiceProvider>
+            <SesionProvider>
+                <CarritoProvider>
+                    {children}
+                </CarritoProvider>
+            </SesionProvider>
+        </ProductoServiceProvider>
+    </UsuarioServiceProvider>
 );
