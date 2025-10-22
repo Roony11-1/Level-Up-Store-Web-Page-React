b66c4899ae8068008ec5149b0412bbf9005040b9
Roony11-1 <eltheasasin@gmail.com>
2025-10-22 20:25:33 -0300

    si

--- src/model/Carrito.ts
+++ src/model/Carrito.ts
@@ -8,22 +8,22 @@ export class Carrito
 {
     id: number;
     items: itemsType[];
-    total: number;
-
     constructor()
     {
         this.id = 1;
         this.items = [{productoId: 1, cantidad: 1}, {productoId: 2, cantidad: 1}, {productoId: 200, cantidad: 500}];
-        this.total = 0;
     }
 
     getId(): number { return this.id; }
     getItems(): itemsType[] { return this.items; }
-    getTotal(): number { return this.total; }
+
+    getCantidadItems(): number 
+    {
+        return this.items.reduce((total, item) => total + item.cantidad, 0);
+    }
 
     setid(id: number) { this.id = id; return this; }
     setItems(items: itemsType[]) { this.items = items; return this; }
-    setTotal(total: number) { this.total = total; return this; }
 
     /**
      * Metodo booleando que me dira si existe o no en el carrito
