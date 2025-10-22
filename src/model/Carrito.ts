b049a29407a6a045b0d88c9c2325105ecfb60f92
Roony11-1 <eltheasasin@gmail.com>
2025-10-22 15:49:32 -0300

    Modelo Carrito

---
+++ src/model/Carrito.ts
@@ -0,0 +1,38 @@
+export interface itemsType
+{
+    productoId: number;
+    cantidad: number;
+}
+
+export class Carrito
+{
+    id: number;
+    items: itemsType[];
+
+    constructor()
+    {
+        this.id = 1;
+        this.items = [{productoId: 1, cantidad: 1}, {productoId: 2, cantidad: 1}, {productoId: 200, cantidad: 500}];
+    }
+
+    getId(): number { return this.id; }
+    getItems(): itemsType[] { return this.items; }
+
+    setid(id: number) { this.id = id; return this; }
+    setItems(items: itemsType[]) { this.items = items; return this; }
+
+    /**
+     * Metodo booleando que me dira si existe o no en el carrito
+     */
+    existeEnElCarrito(productoId: number): boolean
+    {
+        const items = this.getItems();
+
+        const existente = items.find(i => i.productoId === productoId);
+
+        if (existente)
+            return true;
+        else
+            return false;
+    }
+}
