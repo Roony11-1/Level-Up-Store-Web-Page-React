a17870124dcff94c0542d5e987c0c987cca5d872
CHOCOLATIN-12345 <vict.sandovalc@duocuc.cl>
2025-10-23 10:03:41 -0300

    -- importaciones para hacer uso de vitest para los test --

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test:{
+    globals:true,
+    environment:'jsdom',
+    setupFiles:['./src/setupTest.js'],
+    coverage:{
+      enabled:true,
+      provider:'v8',
+      reporter:['html','text','json']
+    }
+  }
 })
