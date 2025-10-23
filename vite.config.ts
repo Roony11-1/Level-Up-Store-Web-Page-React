60e7deabd8f97fbca6d24885f484107d3401faac
Roony11-1 <eltheasasin@gmail.com>
2025-10-23 16:16:12 -0300

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test:{
-    globals:true,
-    environment:'jsdom',
-    setupFiles:['./src/setupTest.js'],
-    coverage:{
-      enabled:true,
-      provider:'v8',
-      reporter:['html','text','json']
-    }
-  }
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTest.js'],
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      reporter: ['html', 'text', 'json'],
+    },
+  },
 })
