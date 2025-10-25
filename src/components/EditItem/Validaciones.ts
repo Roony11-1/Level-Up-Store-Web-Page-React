110d9b0e2bb4bcf08d3bbd60838b484d5dad31c0
Roony11-1 <eltheasasin@gmail.com>
2025-10-25 18:38:20 -0300

    porcentaje

--- src/components/EditItem/Validaciones.ts
+++ src/components/EditItem/Validaciones.ts
@@ -1,8 +1,8 @@
 export class ValidacionesFormulario
 {
-    public static obligatorio(valor: string): string | null 
+    public static obligatorio(valor: any): string | null 
     {
-        if (!valor || valor.trim() === "")
+        if (valor === undefined || valor === null || String(valor).trim() === "")
             return "Este campo es obligatorio";
         return null;
     }
@@ -10,7 +10,14 @@ export class ValidacionesFormulario
     public static validarEmail(email:string): string | null
     {
         if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email))
-            return "El correo electrónico no es válido"
-        return null
+            return "El correo electrónico no es válido";
+        return null;
+    }
+
+    public static numeroPorcentaje(porcentaje: number): string | null
+    {
+        if (( 0 > porcentaje) || (porcentaje > 100))
+            return "Debes ingresar un valor entre 0 y 100";
+        return null;
     }
 }
