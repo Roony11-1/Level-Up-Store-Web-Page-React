98cc25a90f832a85f846eef9a6d00a6424fda8b7
Roony11-1 <eltheasasin@gmail.com>
2025-10-25 18:17:46 -0300

    Podemos renderizar los botones si existe la condicion

--- src/components/admin/AdminPanelBotones.tsx
+++ src/components/admin/AdminPanelBotones.tsx
@@ -2,16 +2,16 @@ import { Boton } from "../Boton/Boton";
 
 interface AdminPanelBotonesProps
 {
-    onDelete: () => void;
-    onEdit: () => void;
+    onDelete?: () => void;
+    onEdit?: () => void;
 }
 
 export function AdminPanelBotones( {onDelete, onEdit}:AdminPanelBotonesProps )
 {
     return(
         <div className="admin-panel-btns">
-            <Boton onClick={onDelete}>Borrar</Boton>
-            <Boton onClick={onEdit}>Editar</Boton>
+            {onDelete && <Boton onClick={onDelete}>Borrar</Boton>}
+            {onEdit && <Boton onClick={onEdit}>Editar</Boton>}
         </div>
     );
 }
