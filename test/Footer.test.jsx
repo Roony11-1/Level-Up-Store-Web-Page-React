0fd4d54100068eb3df50dec1ef4b98331c92f7a4
CHOCOLATIN-12345 <vict.sandovalc@duocuc.cl>
2025-10-28 09:42:50 -0300

    -- se agrega 1 test --

--- test/Footer.test.jsx
+++ test/Footer.test.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { MemoryRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import { describe, test, expect } from "vitest";
 import { Footer } from "../src/components/Footer/Footer"; // ajusta ruta
 
 describe("Footer", () => {
+
 
     test("muestra mensaje de suscripción al presionar Enter y limpia input después", async () => {
     render(
@@ -17,21 +19,37 @@ describe("Footer", () => {
 
     const input = screen.getByPlaceholderText("email@example.com");
 
-    // Escribimos un email
+    
     await userEvent.type(input, "suscriptor@correo.com");
 
-    // Presionamos Enter
+    
     await userEvent.keyboard("{Enter}");
 
     // El mensaje aparece
     expect(screen.getByText("¡Te has suscrito!")).toBeInTheDocument();
 
-    // Esperamos 4 segundos (4000 ms) de manera asíncrona
+    
     await new Promise(r => setTimeout(r, 4000));
 
-    // El mensaje desaparece y el input se limpia
+    
     expect(screen.queryByText("¡Te has suscrito!")).not.toBeInTheDocument();
     expect(input).toHaveValue("");
     });
+
+
+
+    test(' Permite escribir en el campo de email', () => {
+    render(
+      <BrowserRouter>
+        <Footer />
+      </BrowserRouter>
+    );
+
+    const input = screen.getByPlaceholderText('email@example.com');
+    fireEvent.change(input, { target: { value: 'test@correo.com' } });
+
+    expect(input).toHaveValue('test@correo.com');
+  });
+
 
 });
