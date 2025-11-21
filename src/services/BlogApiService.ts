d0fb0774fbf9429e976cfe49ffb3ac2b7bc2c922
Roony11-1 <eltheasasin@gmail.com>
2025-11-21 15:43:48 -0300

    blog

--- src/services/BlogApiService.ts
+++ src/services/BlogApiService.ts
@@ -1,11 +1,10 @@
 import { Blog } from "../model/Blog";
-import { blogController } from "../API/Api";
 import { BaseApiService } from "./BaseApiService";
 
 export class BlogApiService extends BaseApiService<Blog> 
 {
   constructor() 
   {
-    super(blogController, Blog);
+    super("blog", Blog);
   }
 }
